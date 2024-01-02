223e1ef7137b331096e67463c34ad6f57c94d9d7
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2024-01-02 22:09:44 +0530

    Refactoring Constructor

--- mood-analyzer/src/test/java/Junit.java
+++ mood-analyzer/src/test/java/Junit.java
@@ -1,13 +1,39 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 import mood.MoodAnalyzer;
 
 public class Junit {
     @Test
-    public void testMood(){
+    public void test11(){
         MoodAnalyzer analyzer = new MoodAnalyzer();
         
-        String text = "";
-        analyzer.getMood(text);
+        String text = "I Am In Sad Mood";
+        assertEquals(analyzer.getMood(text), "SAD");;
+    }
+
+    @Test
+    public void test12(){
+        MoodAnalyzer analyzer = new MoodAnalyzer();
+        
+        String text = "I Am In Any Mood";
+        assertEquals(analyzer.getMood(text), "HAPPY");;
+    }
+
+    @Test
+    public void test21(){
+        String text = "I Am In Sad Mood";
+        MoodAnalyzer analyzer = new MoodAnalyzer(text);
+        
+        assertEquals(analyzer.getMood(), "SAD");;
+    }
+
+    @Test
+    public void test22(){
+        String text = "I Am In Any Mood";
+        MoodAnalyzer analyzer = new MoodAnalyzer(text);
+        
+        assertEquals(analyzer.getMood(), "HAPPY");;
     }
 }
